ca7ef173e17942e941485f6dcbdbd317518f3e8e
Ryan Locquiao <0579438@my.msjc.edu>
2026-03-25 16:05:39 -0700

    Removed dots on logo.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -25,7 +25,7 @@ export function Navbar({ labels, activeIndex, scrollTo }) {
                 letterSpacing: "0.16rem",
                 color: "var(--offwhite)",
             }}>
-                J<span style={{ color: "var(--red)" }}>.</span>R<span style={{ color: "var(--red)" }}>.</span>L
+                JRL
             </span>
 
             <div style={{ display: "flex", gap: "0.5rem"}}>
